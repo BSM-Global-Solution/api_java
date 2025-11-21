bb5bd2fc872bd1da736e1295a66c4be4db12abd8
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-20 22:40:04 -0300

    feat(UsuarioTO): adicionado o atributo id

--- src/main/java/br/com/evolvere/to/UsuarioTO.java
+++ src/main/java/br/com/evolvere/to/UsuarioTO.java
@@ -4,15 +4,25 @@ import java.util.Date;
 
 public class UsuarioTO {
     // Atributos
+    private int id;
     private String nome;
     private Date dataDeNascimento;
     private String email;
     private String senha;
 
-    // Construtor vazio
+    // Construtores
     public UsuarioTO() {}
 
-    // Construtor com parâmetros
+    // Construtor usado quando os dados já vêm do banco (com ID)
+    public UsuarioTO(int id, String nome, Date dataDeNascimento, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    // Construtor usado para criar o usuário sem ID (antes de salvar)
     public UsuarioTO(String nome, Date dataDeNascimento, String email, String senha) {
         this.nome = nome;
         this.dataDeNascimento = dataDeNascimento;
@@ -21,6 +31,14 @@ public class UsuarioTO {
     }
 
     // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -57,7 +75,8 @@ public class UsuarioTO {
     @Override
     public String toString() {
         return "UsuarioTO{" +
-                "nome='" + nome + '\'' +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
                 ", dataDeNascimento=" + dataDeNascimento +
                 ", email='" + email + '\'' +
                 '}';
